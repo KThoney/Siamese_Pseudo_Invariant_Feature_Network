ac61f8fdde401beedf0488570dbf8fd03d26267e
RS_KThoney <106787991+KThoney@users.noreply.github.com>
2023-02-13 10:32:12 +0900

    Create __init__.py

---
+++ Model/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'Taeheon, Kim'
+__Modelname__ = 'SIPIFnet'
+__credits__ = 'Seoul National University of Science and Technology'
+__License__ = 'Taeheon Kim & Remote Sensing Image Processing Laboratory'
